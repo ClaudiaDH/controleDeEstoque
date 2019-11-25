8cb24d747de1cd6fd6a329573b869ec356ddd371
ClaudiaDH <claudialeite.estudos@gmail.com>
2019-11-25 12:05:47 -0300

    gerando tabela users and products

---
+++ database/migrations/2019_11_25_145221_create_products.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProducts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            //seguindo a DER
+            $table->string('name', 300);
+            $table->string('description', 300);
+            $table->string('quantity');
+            $table->float('price', 10, 2);
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
